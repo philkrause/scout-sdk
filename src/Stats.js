029b5b8da3dfc19cca9c9b8b8bf4d3e175f97264
Philip Krause <philipkrause7@gmail.com>
2019-08-04 22:43:41 -0400

    props

--- src/Stats.js
+++ src/Stats.js
@@ -1,36 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 
-export default function Stats() {
-  const [stats, setStats] = useState()
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setStats(JSON.parse(sessionStorage.getItem('stats')))
-    setLoading(false)
-  }, [])
-  console.log(stats)
-
-  const rt = () => {
-    if (!loading) {
-      return (
-        <>
-          {stats.map(m => {
-            return <h3>{m.value}</h3>
-          })}
-        </>
-      )
-    } else {
-      return (
-        <>
-          <h1>loading...</h1>
-        </>
-      )
-    }
+class Stats extends Component {
+  render() {
+    return (
+      <>
+        <section className="solo-card">
+          <p>{this.props.def}</p>
+          <p>{this.props.totals}</p>
+        </section>
+      </>
+    )
   }
-
-  return (
-    <>
-      <div>{rt()}</div>
-    </>
-  )
 }
+export default Stats
